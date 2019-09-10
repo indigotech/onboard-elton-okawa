6f664dab2817c142d79eba5554d8e24580e0e768
Elton Okawa <elton.okawa@taqtile.com>
2019-09-10 08:37:26 -0300

    fix(cr): save users in one query

--- src/seeds.ts
+++ src/seeds.ts
@@ -5,14 +5,14 @@ dotenv.config();
 import { getConnection } from 'typeorm';
 
 import { startServer } from 'src/server';
-import { addDummyUserOnDb } from 'test/addDummyUserOnDb';
+import { addManyDummyUsersOnDb } from 'test/addDummyUserOnDb';
+import { UserEntity } from 'src/entity/User.entity';
 
 const NUMBER_OF_USERS = 50;
 
 const performSeed = async () => {
-  console.log(process.env.NODE_ENV);
   let httpServer = await startServer();
-  
+
   await populateUsers();
 
   await getConnection().close();
@@ -20,10 +20,8 @@ const performSeed = async () => {
 };
 
 const populateUsers = async () => {
-  for(let i = 0; i < NUMBER_OF_USERS; i++) {
-    let user = await addDummyUserOnDb();
-    console.info(`Added user ${user.name} to database`);
-  }
+  const addedUsers = await addManyDummyUsersOnDb(NUMBER_OF_USERS);
+  addedUsers.forEach((user: UserEntity) => console.info(`Added user ${user.name}`));
 };
 
 performSeed();
